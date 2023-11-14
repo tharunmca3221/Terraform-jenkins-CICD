2a628910ff0095f3a35d4901b9e42f4c4f358281
tharunmca3221 <130220341+tharunmca3221@users.noreply.github.com>
2023-11-14 10:09:25 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "tharun-3221"
     key            = "my-terraform-environment/main"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     dynamodb_table = "mrcloudbook-dynamo-db-table"
   }
 }
