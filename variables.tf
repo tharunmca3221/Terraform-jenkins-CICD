a824ffb159cd3f40eabb6a2fe0df121a107059a6
tharunmca3221 <130220341+tharunmca3221@users.noreply.github.com>
2023-11-14 11:05:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "tharun-3221"
+  default     = "tharun-987"
 }
 
 variable "acl" {
